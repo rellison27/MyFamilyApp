2997fddfc984f0647a5ccd47bb2f75b1fe6166e3
Rashaun Ellison <rellison27@gmail.com>
2023-11-06 09:51:02 -0500

    fully functional

--- app/src/main/java/com/example/myfamily/MainActivity.kt
+++ app/src/main/java/com/example/myfamily/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -19,12 +18,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,13 +57,22 @@ class MainActivity : ComponentActivity()
 @Composable
 fun MyFamily(modifier: Modifier =  Modifier)
 {
+    var iteration by remember { mutableStateOf(0) }
+    var data by remember {
+        mutableStateOf(listOf(R.drawable.myfam0, R.string.my_fam_desc0, R.string.my_fam_year0))
+    }
+    var contentDescription by remember { mutableStateOf(R.string.my_fam_desc0) }
+    var date by remember { mutableStateOf(R.string.my_fam_year0) }
+    var imageTitle by remember { mutableStateOf("Title") }
+    var place by remember { mutableStateOf("Where") }
+
     Column(
         modifier = Modifier.fillMaxHeight(),
         verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Column {
-            Box(
+            Surface(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(450.dp)
@@ -69,12 +82,11 @@ fun MyFamily(modifier: Modifier =  Modifier)
             ) {
                 Image(
                     modifier = Modifier
-                        .align(Alignment.Center)
                         .fillMaxSize()
                         .padding(24.dp),
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit
+                    painter = painterResource(id = data[0]),
+                    contentDescription = stringResource(id = data[1]),
+                    contentScale = ContentScale.FillBounds
                 )
             }
         }
@@ -82,20 +94,24 @@ fun MyFamily(modifier: Modifier =  Modifier)
             modifier = Modifier,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(modifier = Modifier.background(Color.LightGray).padding(16.dp)) {
+            Column(
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .padding(16.dp)
+            ) {
                 Text(
-                    text = "What the photo is",
+                    text = stringResource(id = data[1]),
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Light
                 )
                 Row {
                     Text(
-                        text = "Date",
+                        text = place,
                         fontSize = 16.sp,
                         fontWeight = FontWeight.Bold
                     )
                     Text(
-                        text = "(and where)",
+                        text = "(${stringResource(id = data[2])})",
                     )
                 }
             }
@@ -104,20 +120,40 @@ fun MyFamily(modifier: Modifier =  Modifier)
                     modifier = Modifier
                         .weight(1F)
                         .padding(16.dp),
-                    onClick = { /*TODO*/ }) {
-                    Text("Previous")
+                    onClick = {
+                        if (iteration <= 0) iteration = 0 else iteration--
+                        data = handleButtonTap(iteration)
+                    }
+                ) {
+                    Text(stringResource(R.string.previous))
                 }
                 Button(
                     modifier = Modifier
                         .weight(1F)
                         .padding(16.dp),
-                    onClick = { /*TODO*/ }) {
-                    Text(text = "Next")
+                    onClick = {
+                        if (iteration >= 4 ) iteration = 4 else iteration++
+                        data = handleButtonTap(iteration)
+                    }
+                ) {
+                    Text(text = stringResource(R.string.next))
                 }
             }
         }
     }
+}
 
+ fun handleButtonTap(currentPos: Int): List<Int> {
+    return selectImage(currentPos)
+}
+fun selectImage(currentPos: Int): List<Int> {
+    return when(currentPos) {
+        0 -> listOf(R.drawable.myfam0, R.string.my_fam_desc0, R.string.my_fam_year0)
+        1-> listOf(R.drawable.myfam1, R.string.my_fam_desc1, R.string.my_fam_year1)
+        2 -> listOf(R.drawable.myfam2, R.string.my_fam_desc2, R.string.my_fam_year2)
+        3 -> listOf(R.drawable.myfam3, R.string.my_fam_desc3, R.string.my_fam_year3)
+        else -> listOf(R.drawable.myfam4, R.string.my_fam_desc4, R.string.my_fam_year4)
+    }
 }
 
 @Preview(showBackground = true)
